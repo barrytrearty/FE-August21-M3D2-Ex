560e7292f2227779b1629f409f1d9962dbdd4f53
barrytrearty <btrearty@gmail.com>
2021-08-11 10:28:54 +0100

    FIxed exercise

--- random-user.js
+++ random-user.js
@@ -1,16 +1,18 @@
 "strict mode";
 
-const empty = 1;
-
 const displayUser = function (object) {
   let userDisplayArea = document.getElementById("display-area");
-  userDisplayArea.innerHTML += `<div class="card" style="width: 18rem;">
-<img src="${object.results[0].picture.large}" class="card-img-top" alt="...">
+  //   userDisplayArea.innerHTML +
+
+  let newUser = `<div class="card" style="width: 18rem;">
+<img src="${object[0].picture.large}" class="card-img-top" alt="...">
 <div class="card-body">
-  <h5 class="card-title">${object.results[0].name.first}</h5>
-  <p class="card-text">${object.results[0].location.city}</p>
+  <h5 class="card-title">${object[0].name.first}</h5>
+  <p class="card-text">${object[0].location.city}</p>
 </div>
 </div>`;
+
+  userDisplayArea.insertAdjacentHTML("afterbegin", newUser);
 };
 
 const generateRandomUser = function () {
@@ -18,10 +20,11 @@ const generateRandomUser = function () {
     fetch("https://randomuser.me/api")
       .then((response) => response.json())
       //   .then((response) => console.log(response))
-      .then((response) => {
-        console.log(response);
-        displayUser(response);
+      .then((user) => {
+        console.log(user);
+        displayUser(user.results);
       })
-      .catch((response) => console.log(response))
+      .catch((error) => console.log(error))
   );
 };
+//?results=50
